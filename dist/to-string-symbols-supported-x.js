ba1840941ff8e78e73e74b6a8253ccc788e2a047
Graham Fairweather <xotic750@gmail.com>
2019-07-19 23:48:06 +0200

    :bookmark: v2.0.12
    Auto build

--- dist/to-string-symbols-supported-x.js
+++ dist/to-string-symbols-supported-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-18T18:38:48.627Z",
+  "date": "2019-07-19T21:48:00.945Z",
   "describe": "",
   "description": "ES6 abstract ToString with Symbol conversion support.",
   "file": "to-string-symbols-supported-x.js",
   "hash": "1898953cb1e72cbe2254",
   "license": "MIT",
-  "version": "2.0.11"
+  "version": "2.0.12"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
